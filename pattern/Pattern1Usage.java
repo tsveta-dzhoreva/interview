ab5135520ff47d606464d263d8fb645f702ee3cd
Tsveta Dzhoreva <tsveta.dzhoreva@taulia.com>
2023-11-22 11:23:10 +0200

    Create Pattern1Usage.java

---
+++ pattern/Pattern1Usage.java
@@ -0,0 +1,9 @@
+public class Pattern1Usage {
+   public static void main(String[] args) {
+
+      Pattern1 object = Pattern1.getInstance();
+
+      //show the message
+      object.showMessage();
+   }
+}
